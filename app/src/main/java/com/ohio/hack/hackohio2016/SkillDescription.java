fb7a3f09c7c356a8c2d4aa6d33aaccdb58ebcd9c
Shaina Leibovich <leibovich.2@buckeyemail.osu.edu>
2016-11-19 19:29:15 -0500

    Completed(?) Skill/Description page

--- app/src/main/java/com/ohio/hack/hackohio2016/SkillDescription.java
+++ app/src/main/java/com/ohio/hack/hackohio2016/SkillDescription.java
@@ -1,15 +1,9 @@
 package com.ohio.hack.hackohio2016;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.content.ContextWrapper;
 import android.widget.Button;
 import java.lang.String;
 
@@ -23,7 +17,7 @@ public class SkillDescription extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_skill_description);
 
-        EditText skill = (EditText) findViewById(R.id.skill);
+        EditText skill = (EditText) findViewById(R.id.enter_skill);
         skill.setText(skillString);
 
         EditText description = (EditText) findViewById(R.id.Description);
@@ -34,8 +28,10 @@ public class SkillDescription extends AppCompatActivity {
 
             public void onClick(View view) {
                 EditText description = (EditText) findViewById(R.id.Description);
+                EditText skill = (EditText) findViewById(R.id.enter_skill);
 
                 descriptionString = description.getText().toString();
+                skillString = skill.getText().toString();
             }
         });
     }
