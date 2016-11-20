0c6b678df06cd5e63ae039d62aeaa1e655e5a111
Ismita <516812780@qq.com>
2016-11-19 21:00:39 -0500

    Initial Import of InputInterest and InputSkill Initial Import of Activity_input_interest, Activit_input_skill

--- app/src/main/java/com/ohio/hack/hackohio2016/InputSkill.java
+++ app/src/main/java/com/ohio/hack/hackohio2016/InputSkill.java
@@ -1,5 +1,6 @@
 package com.ohio.hack.hackohio2016;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -17,8 +18,8 @@ public class InputSkill extends AppCompatActivity {
         setContentView(R.layout.activity_input_interest);
 
 
-        Button saveSKiButton = (Button) findViewById(R.id.skiSaveButton);
-        saveSKiButton.setOnClickListener(new View.OnClickListener() {
+        Button ski1 = (Button) findViewById(R.id.skiButton1);
+        ski1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
 //                EditText ski1Text =  (EditText) findViewById(R.id.skiText1);
@@ -32,6 +33,50 @@ public class InputSkill extends AppCompatActivity {
 //                String ski3 = ski3Text.getText().toString();
 //                String ski4 = ski4Text.getText().toString();
 //                String ski5 = ski5Text.getText().toString();
+
+
+
+
+
+            }
+        });
+
+        Button ski2 = (Button) findViewById(R.id.skiButton2);
+        ski2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                startActivity(new Intent(InputSkill.this, SkillDescription.class));
+
+
+
+            }
+        });
+
+        Button ski3 = (Button) findViewById(R.id.skiButton3);
+        ski2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                startActivity(new Intent(InputSkill.this, SkillDescription.class));
+
+
+            }
+        });
+
+        Button ski4 = (Button) findViewById(R.id.skiButton4);
+        ski2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+
+                startActivity(new Intent(InputSkill.this, SkillDescription.class));
+
+            }
+        });
+
+        Button ski5 = (Button) findViewById(R.id.skiButton5);
+        ski2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                startActivity(new Intent(InputSkill.this, SkillDescription.class));
 
 
             }
